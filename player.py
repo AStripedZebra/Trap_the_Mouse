97dd03cb7695df4f79a5a0c285b97f060f611b9a
Jelle <hakimdesultan@gmail.com>
2022-01-17 12:33:43 +0100

    player.py
    update

--- player.py
+++ player.py
@@ -1,18 +1,23 @@
 from pygame import draw
+from pygame import Color
 
 class player():
 
-    x_pos = 0
-    y_pos = 0
+    def __init__(self, player_id, start_pos, screen):
+        self.screen = screen
+        self.player_id = player_id
+        player_pos = start_pos
+        player_color = Color(0, 0, 0)
 
-    def __init__(self, player_id, start_x, start_y):
-        player_id = 0
-        x_pos = start_x
-        y_pos = start_y
+        if player_id == 0:
+            player_color = Color(0, 0, 255) #rgb blue
+        elif player_id == 1:
+            player_color = Color(255, 0, 0) #rgb red
 
-    def move(self, x_pos, y_pos, nunchuck_x, nunchuck_y):
-        x_pos = x_pos + nunchuck_x
-        y_pos = y_pos + nunchuck_y
+    def move(self, x_pos, y_pos, nunchuck_input):
+        nunchuck_input = (0, 0)
+        player_pos = nunchuck_input
 
-    def draw_player():
-        draw.circle
+    def draw_player(self):
+        circle_rad = 20
+        draw.circle(self.screen, self.player_color, self.player_pos, circle_rad)
