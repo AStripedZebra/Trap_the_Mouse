da324be2a391b4272cf53fe2c300ed16422b3094
Jelle <hakimdesultan@gmail.com>
2022-01-22 14:37:08 +0100

    Main & Mouse
    small update

--- mouse.py
+++ mouse.py
@@ -1,17 +1,26 @@
 from pygame import draw
 from pygame import Color
 
+
 class Mouse:
 
-    def __init__(self, start_pos, screen):
+    def __init__(self, start_pos, player1, player2, screen):
         self.screen = screen
         mouse_pos = start_pos
         mouse_color = Color(0, 0, 0)
 
+        p1 = player1
+        p2 = player2
+
     def move(self, x_pos, y_pos, nunchuck_input):
-        nunchuck_input = (0, 0)
-        mouse_pos = nunchuck_input
+        pass
 
     def draw_player(self):
         circle_rad = 20
         draw.circle(self.screen, self.mouse_color, self.mouse_pos, circle_rad)
+
+    def escape_AI(self):
+        pass
+
+    def get_possible_moves(self, maze):
+        pass
