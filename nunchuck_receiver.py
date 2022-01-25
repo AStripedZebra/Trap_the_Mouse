03c2de59c7fae902c1cd04c0dcdfd6d2823e7efb
Jelle <hakimdesultan@gmail.com>
2022-01-25 14:42:15 +0100

    Check Pycache
    test

--- nunchuck_receiver.py
+++ nunchuck_receiver.py
@@ -3,15 +3,14 @@ import serial
 
 
 class Nunchuck_receiver:
-
     def __init__(self):
         self.arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+        
 
     def request_data(self):
         self.arduino.write(bytes('r', 'utf-8'))
 
     def retrieve_data(self):
-
         data = str(self.arduino.readline())
         print(data)
         return data
