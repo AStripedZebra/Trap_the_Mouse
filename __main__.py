6cfcd0ba949a430ee654fb8d293ad38ca2d0955d
Ruben <rubenkoudijs0@gmail.com>
2022-01-25 14:42:57 +0100

    Auto stash before merge of "main" and "origin/main"

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 import keyboard
 from player import Player
-#from nunchuck_receiver import Nunchuck_receiver
+from nunchuck_receiver import Nunchuck_receiver
 from mouse import Mouse
 from maze import Maze
 from pygame.locals import *
@@ -15,10 +15,10 @@ fpsClock = pygame.time.Clock()
 width, height = 640, 640
 screen = pygame.display.set_mode((width, height))
 maze = Maze()
-#nun_rec = Nunchuck_receiver()
+nun_rec = Nunchuck_receiver()
 
-p1 = Player(0, (0, 0), maze)#, nun_rec)
-p2 = Player(1, (15, 0), maze)#, nun_rec)
+p1 = Player(0, (0, 0), maze, nun_rec)
+p2 = Player(1, (15, 0), maze, nun_rec)
 mouse = Mouse((15, 15), maze)
 
 
@@ -32,7 +32,7 @@ while True:
             sys.exit()
 
     # Update.
-    #mouse.new_path()
+    mouse.new_path()
     mouse.move(p1, p2)
     p1.update()
     p2.update()
--- nunchuck_receiver.py
+++ nunchuck_receiver.py
@@ -4,8 +4,12 @@ import serial
 
 class Nunchuck_receiver:
     def __init__(self):
+<<<<<<< Updated upstream
         self.arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
         
+=======
+        self.arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+>>>>>>> Stashed changes
 
     def request_data(self):
         self.arduino.write(bytes('r', 'utf-8'))
